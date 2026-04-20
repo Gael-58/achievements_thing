6303d9f07c45db6f47ac1c45292f849be161b252
Gael-58 <92817546+Gael-58@users.noreply.github.com>
2026-04-20 09:33:59 +0200

    Update getData.js

--- src/js/getData.js
+++ src/js/getData.js
@@ -1,9 +1,9 @@
-  export async function getData(route) {
-    try {
-      const response = await fetch(new Request(route));
-      const output = await response.json();
-      return output;
-    } catch (error) {
-      console.error("error: " + error);
-    }
+export async function getData(route) {
+  try {
+    const response = await fetch(new Request(route));
+    const output = await response.json();
+    return output;
+  } catch (error) {
+    console.error("error: " + error);
   }
+}
