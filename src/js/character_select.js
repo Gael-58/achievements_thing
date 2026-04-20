8329a3cc7a3ee42eafec102caea742ce11ed95ac
Gael-58 <92817546+Gael-58@users.noreply.github.com>
2026-04-20 09:26:20 +0200

    Update character_select.js

--- src/js/character_select.js
+++ src/js/character_select.js
@@ -1,16 +1,15 @@
 import { getData } from 'https://gael-58.github.io/achievements_thing/src/js/getData.js';
 
-export async function getCharacterSelector(characterSelector, route){
+export async function getCharacterSelector(characterSelector, route) {
   const urlCharacters = route;
   characterSelector.id = "characterSelect";
   // characterSelector.multiple = true;
-    
-  let data = await getData(urlCharacters);
-    for (let key in data) {
-      const listOption = document.createElement("option");
-      listOption.value = key;
-      listOption.innerText = data[key].display_name;
-      characterSelector.appendChild(listOption.cloneNode(true));
-    }
-}
 
+  let data = await getData(urlCharacters);
+  for (let key in data) {
+    const listOption = document.createElement("option");
+    listOption.value = key;
+    listOption.innerText = data[key].display_name;
+    characterSelector.appendChild(listOption.cloneNode(true));
+  }
+}
