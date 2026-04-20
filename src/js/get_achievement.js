a3c081f73ff15913f9946a561bc19bb0507e8117
Gael-58 <92817546+Gael-58@users.noreply.github.com>
2026-04-20 09:34:32 +0200

    Update get_achievement.js

--- src/js/get_achievement.js
+++ src/js/get_achievement.js
@@ -1,11 +1,11 @@
 import { getData } from 'https://gael-58.github.io/achievements_thing/src/js/getData.js';
 
-export async function getAchievement(route, ...keys){
+export async function getAchievement(route, ...keys) {
     const data = route;
     let output = await getData(data);
     // console.log("getAchievement keys = " + keys.toString());
-    
-    for(let key of keys){
+
+    for (let key of keys) {
         output = output[key];
     }
     // console.log("getAchievement output = " + JSON.stringify(output));
